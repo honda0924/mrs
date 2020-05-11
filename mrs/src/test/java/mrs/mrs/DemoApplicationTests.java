e89369f82d460caaea4ee727beb4efc897728920
honda0924 <honda0924@hotmail.co.jp>
2020-05-11 21:51:35 +0900

    Initial commit

---
+++ mrs/src/test/java/mrs/mrs/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mrs.mrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
