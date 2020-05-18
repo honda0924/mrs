973f4465b761e1a9ee9c2a1f7310095d206575d2
honda0924 <honda0924@hotmail.co.jp>
2020-05-18 21:19:34 +0900

    create_ReservableRoomRepository

---
+++ mrs/src/main/java/mrs/domain/repository/room/ReservableRoomRepository .java
@@ -0,0 +1,17 @@
+package mrs.domain.repository.room;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import mrs.domain.model.ReservableRoom;
+import mrs.domain.model.ReservableRoomId;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * ReservableRoomRepository 
+ */
+public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, ReservableRoomId> {
+  List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(LocalDate reservedDate);
+  
+}
